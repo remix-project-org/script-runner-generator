677eec06f234940859fd09b8749d9275608adcc5
bunsenstraat <filip.mertens@ethereum.org>
2025-01-30 15:48:27 +0100

    add test

---
+++ test/src/tests/noir1.test.ts
@@ -0,0 +1,15 @@
+'use strict'
+import { NightwatchBrowser } from 'nightwatch'
+import init from '../helpers/init'
+
+module.exports = {
+    before: function (browser: NightwatchBrowser, done: VoidFunction) {
+        init(browser, done, 'http://127.0.0.1:8080?plugins=solidity,udapp', false, {
+            name: 'scriptRunner',
+            url: 'http://127.0.0.1:3000?template=noir1',
+        })
+    },
+    'Should exec a noir1 TS file': function (browser: NightwatchBrowser) {
+        
+    }
+}
